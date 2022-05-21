ab115cdd1dc1850000f39e584bd766ace4408d3a
Wren McQueary <wren.mcqueary@gmail.com>
2022-05-21 19:52:23 -0400

    Added support for chaos (automatic random drift in tempo)(joke feature)

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ Command line arguments are as follows:
 -t, --tempo:        tempo in bpm
 -p, --pitch:        pitch of beeps as a note string (eg A4)
 -d, --duration:     duration of each beep, in ms
--s, --signature:    time signature
+-s, --signature:    time signature numerator (single integer)
+-c, --chaos:        automatic random drift in tempo (joke option)
 """
 
 
 # TODO: Play a little bit of background noise so that other systems on the user's machine don't mute the beeps for being too short
-# TODO: Chaos option
+# TODO: Write readme
 
 
 import argparse
 import musicalbeeps
 from time import sleep, time
+from random import uniform
 
 
 if __name__ == "__main__":
@@ -23,11 +25,13 @@ if __name__ == "__main__":
     parser.add_argument("--pitch", "-p", action="store", type=str, required=False, default="A4", help="pitch of emphasized beeps as a note string (eg A4)", dest="pitch")
     parser.add_argument("--duration", "-d", action="store", type=int, required=False, default=50, help="duration of each beep in ms", dest="duration")
     parser.add_argument("--signature", "-s", action="store", type=int, required=False, default=1, help="time signature numerator (single integer)", dest="signature")
+    parser.add_argument("--chaos", "-c", action="store_true", required=False, default=False, help="automatic random drift in tempo (joke option)", dest="chaos")
     args = parser.parse_args()
     tempo = args.tempo
     pitch = args.pitch
     duration = args.duration
     signature = args.signature
+    chaos = args.chaos
 
     # Handle errors
     # duration not shorter than tempo
@@ -53,6 +57,9 @@ if __name__ == "__main__":
             pitch_to_play = pitch_unemphasized
         counter = (counter + 1) % signature
         player.play_note(pitch_to_play, duration_seconds)
+        # Drift period if chaos
+        if chaos:
+            period += uniform(-0.1, 0.1)
 
         # Wait until it's time to play another note
         while time() - time_before < period:
